ba5826cb05cce977d706115ffd42d0238014972b
Sachin K Singh <sachin.kumar.singh@accenture.com>
2020-05-11 22:51:20 +0100

    Initial Commit

---
+++ terraform/base-network/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "minikube" {
+    name = "minikube"
+}
+
+resource "aws_subnet" "minikube_subnet_1" {
+    name = "minikube"
+}
+
+resource "aws_subnet" "minikube_subnet_2" {
+    name = "minikube"
+}
+
+resource "aws_subnet" "minikube_subnet_3" {
+    name = "minikube"
+}
+
